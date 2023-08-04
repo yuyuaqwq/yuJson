d9e7b485d9a19257006626ef353a75cfcc4c15b1
yuyuaqwq <1454832774@qq.com>
2023-08-04 14:33:17 +0800

    .

--- json.hpp
+++ json.hpp
@@ -206,7 +206,9 @@ public:
       return m_value->GetNumber().GetInt();
     }
     case value::ValueType::kString: {
-      return YUJSON_STD stoll(m_value->GetString().Get());
+      auto& str = m_value->GetString().Get();
+      if (str.empty() || str[0] < '0' && str[0] > '9') return 0;
+      return YUJSON_STD stoll(str);
     }
     case value::ValueType::kNull: {
       return defalut;
